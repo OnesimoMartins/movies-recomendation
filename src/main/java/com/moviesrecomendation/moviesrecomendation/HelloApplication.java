35ee13717de1b39f3b68b16747c170fead1f8c97
Onésimo Martins <onesimo.martins@memory.com.br>
2024-03-27 00:25:42 +0100

    chore: first commit

---
+++ src/main/java/com/moviesrecomendation/moviesrecomendation/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.moviesrecomendation.moviesrecomendation;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api-2")
+public class HelloApplication extends Application {
+
+}
